f9a7221f8e7acfe5ddd2ff8809fd2386c2fc3855
Grub <franck.romano@protonmail.com>
2019-10-07 22:15:01 +0200

    chore: add integration test on http.request()

--- tests/outbound-tracer.itest.ts
+++ tests/outbound-tracer.itest.ts
@@ -4,22 +4,60 @@ import { sinon, expect } from './config';
 import OutboundTracer from '../src/outbound-tracer';
 
 describe('Outbound Tracer', () => {
+  let fakeServer;
+  before(() => {
+    fakeServer = server.listen(8081);
+  });
+
+  after(() => {
+    fakeServer.close();
+  });
+
   describe('.(logFn)', () => {
     context('http', () => {
-      const fakeServer = server.start();
-      after(() => fakeServer.close());
-      it('logs the emitted event using the provided { logFn }', async () => {
-        // GIVEN
-        const logFn = sinon.spy();
-        const outboundTracer = new OutboundTracer(logFn);
-        // WHEN
-        outboundTracer.observe();
-        http.get('http://localhost:8081/foo', () => {
-          // THEN
-          expect(logFn).to.have.been.calledWith({});
-          outboundTracer.removeAllListeners();
+      context('http.get()', () => {
+        it('logs the emitted event using the provided { logFn }', async () => {
+          // GIVEN
+          const logFn = sinon.spy();
+          const outboundTracer = new OutboundTracer(logFn);
+          // WHEN
+          outboundTracer.observe();
+          http
+            .get('http://localhost:8081/foo', res => {
+              res.on('data', () => {});
+              expect(logFn).to.have.been.calledWith({});
+            })
+            .on('error', () => {});
+        });
+      });
+
+      context('http.request()', () => {
+        ['GET', 'PUT', 'DELETE', 'POST'].forEach(method => {
+          it('logs the emitted event using the provided { logFn }', async () => {
+            // GIVEN
+            const logFn = sinon.spy();
+            const outboundTracer = new OutboundTracer(logFn);
+            // WHEN
+            outboundTracer.observe();
+            const req = http
+              .request({ host: 'localhost', port: 8081, path: '/foo', method }, res => {
+                res.on('data', () => {});
+                res.on('end', () => {
+                  expect(logFn).to.have.been.calledWith({});
+                });
+              })
+              .on('error', () => {});
+
+            req.write('end', () => {
+              req.end();
+            });
+          });
         });
       });
     });
   });
 });
+
+function req(options) {
+  return new Promise((resolve, reject) => {});
+}
--- tests/server/index.ts
+++ tests/server/index.ts
@@ -2,17 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use((_, res, next) => {
-  res.setHeader('Date', new Date().toUTCString());
-  return next();
-});
-
 app.get('/foo', (_, res) => res.send('bar'));
 app.post('/foo', (_, res) => res.send('bar'));
 app.put('/foo', (_, res) => res.send('bar'));
 app.delete('/foo', (_, res) => res.sendStatus(200));
-app.get('/foo', (_, res) => res.redirect('/foo'));
+app.get('/foo-redirect', (_, res) => res.redirect('/foo'));
 
-export default {
-  start: () => app.listen(8081)
-};
+export default app;
