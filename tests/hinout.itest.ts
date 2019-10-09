73b31c2d2f3dca8a9fdb453545fe8b8b47a3a06f
Franck Romano <franck.romano@protonmail.com>
2019-10-09 11:59:20 +0200

    test: add tests on http error handling

--- tests/hinout.itest.ts
+++ tests/hinout.itest.ts
@@ -8,66 +8,126 @@ describe('Hinout', () => {
   before(() => (fakeServer = server.listen(8081)));
   after(() => fakeServer.close());
 
-  describe('.(logFn)', () => {
+  describe('.({ logFn })', () => {
     const host = 'localhost';
     const port = 8081;
     const path = '/foo';
+    const errorPath = '/bar'
     const expectedHost = `${host}:${port}`;
 
     context('http', () => {
-      context('http.get()', () => {
-        it('logs inbound an outbound request', async () => {
-          // GIVEN
-          const logFn = sinon.spy();
-          new Hinout({ logFn }).observe();
-          // WHEN
-          http
-            .get(`http://${host}:${port}${path}`, result => {
-              result.on('data', () => { });
-              expect(logFn.getCall(0)).to.have.been.calledWith({ host: expectedHost, method: 'GET', path: '/foo' });
-              expect(logFn.getCall(1)).to.have.been.calledWith({
-                httpVersion: '1.1',
-                statusCode: 200,
-                statusMessage: 'OK'
-              });
-            })
-            .on('error', () => { });
-        });
-      });
+      context('sucess', () => {
+        context('.get(url)', () => {
+          it('logs inbound an outbound request', async () => {
+            // GIVEN
+            const logFn = sinon.spy();
+            new Hinout({ logFn }).observe();
+            // WHEN
+            http
+              .get(`http://${host}:${port}${path}`, result => {
+                result.on('data', () => { });
+                expect(logFn.getCall(0)).to.have.been.calledWith({ host: expectedHost, method: 'GET', path });
+                expect(logFn.getCall(1)).to.have.been.calledWith({
+                  httpVersion: '1.1',
+                  statusCode: 200,
+                  statusMessage: 'OK'
+                });
+              })
+              .on('error', () => { });
+          });
+        })
 
-      context('http.request()', () => {
-        [
-          { method: 'GET', statusCode: 200, statusMessage: 'OK' },
-          { method: 'POST', statusCode: 201, statusMessage: 'Created' },
-          { method: 'PUT', statusCode: 200, statusMessage: 'OK' },
-          { method: 'DELETE', statusCode: 200, statusMessage: 'OK' }
-        ].forEach(({ method, statusCode, statusMessage }) => {
-          context(`HTTP method: ${method}`, () => {
-            it('logs inbound an outbound request', async () => {
-              // GIVEN
-              const logFn = sinon.spy();
-              new Hinout({ logFn }).observe();
-              // WHEN
-              const req = http
-                .request({ host, port, path, method }, result => {
-                  result.on('data', () => { });
-                  result.on('end', () => {
-                    expect(logFn).to.have.been.calledWith({ host: expectedHost, method, path });
-                    expect(logFn).to.have.been.calledWith({
-                      httpVersion: '1.1',
-                      statusCode,
-                      statusMessage
+        context('.request(options)', () => {
+          [
+            { method: 'GET', statusCode: 200, statusMessage: 'OK' },
+            { method: 'POST', statusCode: 201, statusMessage: 'Created' },
+            { method: 'PUT', statusCode: 200, statusMessage: 'OK' },
+            { method: 'DELETE', statusCode: 200, statusMessage: 'OK' }
+          ].forEach(({ method, statusCode, statusMessage }) => {
+            context(`HTTP method: ${method}`, () => {
+              it('logs inbound an outbound request', async () => {
+                // GIVEN
+                const logFn = sinon.spy();
+                new Hinout({ logFn }).observe();
+                // WHEN
+                const req = http
+                  .request({ host, port, path, method }, result => {
+                    result.on('data', () => { });
+                    result.on('end', () => {
+                      expect(logFn).to.have.been.calledWith({ host: expectedHost, method, path });
+                      expect(logFn).to.have.been.calledWith({
+                        httpVersion: '1.1',
+                        statusCode,
+                        statusMessage
+                      });
+                      logFn.resetHistory();
                     });
-                    logFn.resetHistory();
-                  });
-                })
-                .on('error', () => { });
+                  })
+                  .on('error', () => { });
 
-              req.write('end', () => {
-                req.end();
+                req.write('end', () => {
+                  req.end();
+                });
               });
             });
           });
+        });
+      });
+
+      context('error', () => {
+        context('http.get()', () => {
+          it('logs inbound an outbound request', async () => {
+            // GIVEN
+            const logFn = sinon.spy();
+            new Hinout({ logFn }).observe();
+            // WHEN
+            http
+              .get(`http://${host}:${port}${errorPath}`, result => {
+                result.on('data', () => { });
+                expect(logFn.getCall(0)).to.have.been.calledWith({ host: expectedHost, method: 'GET', path: errorPath });
+                expect(logFn.getCall(1)).to.have.been.calledWith({
+                  httpVersion: '1.1',
+                  statusCode: 400,
+                  statusMessage: 'Bad Request'
+                });
+              })
+          });
+        });
+
+
+        context('http.request()', () => {
+          [
+            { method: 'GET', statusCode: 400, statusMessage: 'Bad Request' },
+            { method: 'POST', statusCode: 401, statusMessage: 'Unauthorized' },
+            { method: 'PUT', statusCode: 404, statusMessage: 'Not Found' },
+            { method: 'DELETE', statusCode: 403, statusMessage: 'Forbidden' }
+          ]
+            .forEach(({ method, statusCode, statusMessage }) => {
+              context(`HTTP method: ${method}`, () => {
+                it('logs inbound an outbound request', () => {
+                  // GIVEN
+                  const logFn = sinon.spy();
+                  new Hinout({ logFn }).observe();
+                  // WHEN
+                  const req = http
+                    .request({ host, port, path: errorPath, method }, result => {
+                      result.on('data', () => { });
+                      result.on('end', () => {
+                        expect(logFn).to.have.been.calledWith({ host: expectedHost, method, path: errorPath });
+                        expect(logFn).to.have.been.calledWith({
+                          httpVersion: '1.1',
+                          statusCode,
+                          statusMessage
+                        });
+                      });
+                    })
+
+                  req.write('end', () => {
+                    req.end();
+                  });
+                })
+              });
+            });
         });
       });
     });
--- tests/server/index.ts
+++ tests/server/index.ts
@@ -7,4 +7,9 @@ app.post('/foo', (_, res) => res.sendStatus(201));
 app.put('/foo', (_, res) => res.send('bar'));
 app.delete('/foo', (_, res) => res.sendStatus(200));
 
+app.get('/bar', (_, res) => res.sendStatus(400));
+app.post('/bar', (_, res) => res.sendStatus(401));
+app.put('/bar', (_, res) => res.sendStatus(404));
+app.delete('/bar', (_, res) => res.sendStatus(403));
+
 export default app;
