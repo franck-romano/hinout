ab3b27529e6ab48f2a23d868b1db620eb94f19c5
franck-romano <franck.romano@protonmail.com>
2021-11-18 16:10:01 +0100

    chore(export): use export default

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import Hinout from './src/Hinout';
 import { EventHandler } from './src/infrastructure/EventHandler';
 
-export = new Hinout({ logFn: console.log, eventHandler: new EventHandler() });
+const hinout = new Hinout({ logFn: console.log, eventHandler: new EventHandler() });
+
+export default hinout;
